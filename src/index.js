461837d400630dfa23c5d5189887da14a98a1f56
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-02-04 14:56:27 +0900

    追加作成

--- src/index.js
+++ src/index.js
@@ -12,6 +12,27 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+
+  // liを生成
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
+  li.appendChild(div);
+
+  // pの生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  li.appendChild(p);
+
+  // ul.appendChild(li);
+  const el = document.querySelector(".aaa");
+  console.log(el);
+  el.appendChild(li);
+
+  // div配下にpタグを生成
+
+  // 未完了のリストに追加
+  // alert(inputText);
 };
 
 document
