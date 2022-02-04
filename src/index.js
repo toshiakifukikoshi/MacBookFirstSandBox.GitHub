d50f21b383e512dfa8b3dcfb011b5cb3d26a2b68
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-02-04 19:26:17 +0900

    JavaScriptでTODOリスト完成

--- src/index.js
+++ src/index.js
@@ -8,33 +8,99 @@ import "./styles.css";
 //   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 // </div>
 // `;
+const ul = document.querySelector(".aaa");
 
-const onClickAdd = () => {
-  const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
+const creatEl = (addtext) => {
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.className = "list-row";
 
+  // pの生成
+  const p = document.createElement("p");
+  p.innerText = addtext;
+  const ul = document.querySelector(".kkk");
+
+  ul.appendChild(li);
+  li.appendChild(div);
+  div.appendChild(p);
+  return div;
+};
+
+// 完了エリアに表示イベント
+const addList = (text) => {
+  console.log(text);
+  const div = creatEl(text);
+  const returnButton = document.createElement("button");
+  returnButton.textContent = "戻る";
+  div.append(returnButton);
+
+  // 戻るボタンが押された時の処理
+  returnButton.addEventListener("click", () => {
+    const Target1 = returnButton.parentNode.parentNode;
+    const texts = returnButton.previousElementSibling.textContent;
+
+    const element = document.querySelector(".kkk");
+    element.removeChild(Target1);
+
+    createIncompleteList(texts);
+
+    // deleteFunc(deleteButton, ul);
+  });
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // liを生成
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
-  li.appendChild(div);
 
   // pの生成
   const p = document.createElement("p");
-  p.innerText = inputText;
-  li.appendChild(p);
+  p.innerText = text;
 
-  // ul.appendChild(li);
-  const el = document.querySelector(".aaa");
-  console.log(el);
-  el.appendChild(li);
+  ul.appendChild(li);
+  li.appendChild(div);
+  div.appendChild(p);
 
-  // div配下にpタグを生成
+  //完了ボタン作成
+  const completeButton = document.createElement("button");
+  completeButton.textContent = "完了";
+  div.append(completeButton);
 
-  // 未完了のリストに追加
-  // alert(inputText);
+  // 完了ボタンイベント
+  completeButton.addEventListener("click", () => {
+    // 要素をなくす
+
+    const addTarget = completeButton.previousElementSibling;
+
+    deleteFunc(deleteButton, ul);
+    addList(addTarget.textContent);
+  });
+
+  // 削除ボタン作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "削除";
+  div.append(deleteButton);
+
+  deleteButton.addEventListener("click", () => {
+    deleteFunc(deleteButton, ul);
+    // 要素をなくす
+  });
+};
+
+// 最初の入力画面
+const onClickAdd = (text) => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
 };
 
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
+
+// イベントで未完了リストからの削除関数
+const deleteFunc = (deleteButton, ul) => {
+  ul.removeChild(deleteButton.parentNode.parentNode);
+};
